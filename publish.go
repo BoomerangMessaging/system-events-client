a4d40a99aaae31fe47a3806ab4e1d11a3dabb28e
Piotr Koper <pioterkoper@gmail.com>
2026-04-23 09:57:36 +0100

    Add TODO for extending Worker to support task publishing

--- publish.go
+++ publish.go
@@ -1,4 +1,5 @@
 package systemeventslisten
 
+// TODO: extend Worker to support publishing tasks to queues, e.g. for retrying failed tasks or for sending notifications to other services
 // TODO: add methods to declare queues and exchanges to RabbitMQ, e.g. DeclareQueue(queueName string) error, DeclareExchange(exchangeName string) error
 // TODO: add methods to publish tasks to queue to RabbitMQ, e.g. QueueTask(task Task) error
