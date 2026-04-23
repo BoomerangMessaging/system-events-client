fe715332c6b10ede256716ce399a83d009ebe7f6
Piotr Koper <pioterkoper@gmail.com>
2026-04-23 09:54:56 +0100

    Add initial publish.go file with TODOs for RabbitMQ methods

---
+++ publish.go
@@ -0,0 +1,4 @@
+package systemeventslisten
+
+// TODO: add methods to declare queues and exchanges to RabbitMQ, e.g. DeclareQueue(queueName string) error, DeclareExchange(exchangeName string) error
+// TODO: add methods to publish tasks to queue to RabbitMQ, e.g. QueueTask(task Task) error
