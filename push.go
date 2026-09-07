45667c9ca5c6606896d90b14df61d3c2f3fa38d3
Piotr Koper <pioterkoper@gmail.com>
2026-09-07 16:41:57 +0100

    feat: implement SendNotification method in PushClient for event notifications

--- push.go
+++ push.go
@@ -224,3 +224,59 @@ func (pc *PushClient) SendEvent(ctx context.Context, event *Event) error {
 	slog.Info("event_sent_successfully", "event", event.UniqueKey)
 	return nil
 }
+
+func (pc *PushClient) SendNotification(ctx context.Context, event *Event) error {
+
+	// Implement the logic to send the event to the external system
+	if pc.baseURL == "" {
+		slog.Info("push_client_base_url_is_empty_skipping_event_send", "event", event)
+		return nil
+	}
+
+	if event.Namespace == "" {
+		event.Namespace = pc.namespace
+	}
+
+	if event.UniqueKey == "" {
+		event.GenerateUniqueKey()
+	}
+
+	slog.Debug("sending_notification", "event", event)
+	// marshal event to JSON
+	// send HTTP POST request to pc.baseURL with the event data
+	bytesBuffer := &bytes.Buffer{}
+	err := json.NewEncoder(bytesBuffer).Encode(event)
+	if err != nil {
+		slog.Error("failed_to_marshal_event_to_json", "err", err)
+		return err
+	}
+
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, pc.timeout)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctxWithTimeout, "POST", pc.baseURL+"/notifications", bytesBuffer)
+	if err != nil {
+		slog.Error("failed_to_create_http_request", "err", err)
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		slog.Error("failed_to_send_http_request", "err", err)
+		return err
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		body, err2 := io.ReadAll(resp.Body)
+		if err2 != nil {
+			slog.Error("failed_to_read_response_body", "err", err2)
+		}
+
+		slog.Error("received_non_ok_response", "status_code", resp.StatusCode, "body", string(body))
+		return fmt.Errorf("non-ok response: %d", resp.StatusCode)
+	}
+
+	slog.Info("notification_sent_successfully", "event", event.UniqueKey)
+	return nil
+}
